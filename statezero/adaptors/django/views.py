10dc026d60d91ced2777b5551ba5e61b013b27a9
robbie1540 <109600790+robairhome@users.noreply.github.com>
2025-06-12 16:03:19 +0200

    Update views.py

--- statezero/adaptors/django/views.py
+++ statezero/adaptors/django/views.py
@@ -319,7 +319,7 @@ class FastUploadView(APIView):
         """Get S3 client"""
         import boto3
         return boto3.client(
-            's3',
+            "s3",
             region_name=settings.AWS_S3_REGION_NAME,
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
